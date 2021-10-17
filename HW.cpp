1f1cd3fc1b08a9f7798f880c7316f172ae0262d3
Parksogang <pkj6962@naver.com>
2021-10-17 15:07:04 +0900

    branch1 modified

--- HW.cpp
+++ HW.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(void){
 	int m, b1, b2, b3, sum = 0; 
 	cout << "master branch";
-	for(int i = 0; i < m; i++){
+	cout << "branch1"
+	for(int i = 0; i < b1; i++){
 		sum += i; 
 	} 
 }
