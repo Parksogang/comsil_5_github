7b7393e200ee40e3049fd7de1e27c5152b6a30e9
Parksogang <pkj6962@naver.com>
2021-10-17 13:31:56 +0900

    5 week assignmnet

---
+++ 5week_HW.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespacd std;
+int main(void){
+	int num = 5, sum = 0;
+	cout << "Hello World";
+	for(int i = 0; i < num; i++){
+		sum += i; 
+	}
+	cout << sum; 
+	return 0; 
+}
