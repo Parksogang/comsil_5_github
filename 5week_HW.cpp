d455395d3a959d66443c87f72445a2f6bcb69924
Parksogang <pkj6962@naver.com>
2021-10-17 14:48:09 +0900

    experiment

--- 5week_HW.cpp
+++ 5week_HW.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 using namespacd std;
 int main(void){
-	int num = 15, sum = 0;
-	cout << "Hello World! branch1 has been changed once again.";
-	for(int i = 0; i < num; i++){
-		sum += i; 
-	}
-	cout << sum; 
-	return 0; 
+
 }
