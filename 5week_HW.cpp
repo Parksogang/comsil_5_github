c88cc5d1246ec9371a9f5932ebef82846cd0f732
Parksogang <pkj6962@naver.com>
2021-10-17 14:11:48 +0900

    branch1

--- 5week_HW.cpp
+++ 5week_HW.cpp
@@ -2,7 +2,7 @@
 using namespacd std;
 int main(void){
 	int num = 15, sum = 0;
-	cout << "Hello World. branch2 has modified the file.";
+	cout << "Hello World. branch1 has been changed once again.";
 	for(int i = 0; i < num; i++){
 		sum += i; 
 	}
