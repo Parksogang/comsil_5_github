b295dfe70145bb7739e154201122412278408f61
Parksogang <pkj6962@naver.com>
2021-10-17 15:17:50 +0900

    branch2 modified

---
+++ HW.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+	int m, b1, b2, b3, sum = 0; 
+	cout << "master branch";
+	cout << "branch1"; 
+	cout << "branch2";
+	for(int i = 0; i < b2; i++){
+		sum += i; 
+	} 
+}
