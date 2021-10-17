0a47007ee81aa516011ff143a7ea5cb8e6412610
Parksogang <pkj6962@naver.com>
2021-10-17 13:59:09 +0900

    master modified

--- 5week_HW.cpp
+++ 5week_HW.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespacd std;
 int main(void){
-	int num = 10, sum = 0;
-	cout << "Hello World";
+	int num = 15, sum = 0;
+	cout << "Hello World. Master is modified";
 	for(int i = 0; i < num; i++){
 		sum += i; 
 	}
